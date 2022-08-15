0ff26e14e2a5b2ed0918b8360c965fde7ae8ca9e
bthol <95544562+bthol@users.noreply.github.com>
2022-08-15 09:52:22 -0700

    body-parser intall

--- Controls/controlScorebaord.js
+++ Controls/controlScorebaord.js
@@ -28,7 +28,52 @@ router.get(`/`, async (req, res) => {
     }
 })
 
+//Read route
+router.get(`/:id`, async (req, res) => {
+    try {
+        res.send("read route accessed.")
+    } catch (err) {
+        res.send({
+            error: err
+        })
+    }
+})
 
+//Create route
+router.post(`/`, async (req, res) => {
+    // res.send("create route accessed.")
+    try {
+        const entry = await scoreboard.create(req.body);
+        res.send({
+            entry: entry
+        })
+    } catch (err) {
+        res.send({
+            error: err
+        })
+    }
+})
 
+//Update route
+router.patch(`/:id`, async (req, res) => {
+    try {
+        res.send("update route accessed.")
+    } catch (err) {
+        res.send({
+            error: err
+        })
+    }
+})
+
+//Delete route
+router.delete(`/:id`, async (req, res) => {
+    try {
+        res.send("delete route accessed.")
+    } catch (err) {
+        res.send({
+            error: err
+        })
+    }
+})
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+//Dependencies
 const express = require('Express');
 const server = express();
 const port = 3000;
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+//Middleware
+server.use(bodyParser.urlencoded({ extended: true }));
 
 //Landing route
 server.get(`/`, (req, res) => {
     res.send("Landing route accessed. /scoreboard for top 10 scores and their names")
 })
 
+//Controller Indices
 const scoreboard = require('./Controls/controlScorebaord');
 server.use(`/scoreboard`, scoreboard);
 
@@ -15,6 +21,7 @@ server.use(`/scoreboard`, scoreboard);
 mongoose.connection.on(`error`, (error) => console.error(error));
 mongoose.connection.once(`open`, () => console.log("MongoDB connected"));
 
+//Listener
 server.listen(port, () => {
     console.log(`Express.js is online and running on port: ${port}.`)
 })
